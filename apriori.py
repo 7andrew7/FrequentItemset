4c7f532f45c5dd0d2c764abe781eea4f33868b34
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-17 09:31:51 -0800

    apriori python scaffolding

---
+++ apriori.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import itertools
+
+def apriori(baskets, support):
+    """Compute frequent itemsets using the apriori algorithm.
+
+    :param baskets: A dictionary mapping from a basket ID to an item set.
+    Basket IDs and item IDs must be immutable and hashable.
+    :param support: Support level, expressed as as a percentage of baskets.
+
+    :return: A list of frequent item sets.  Element k contains a list of sets
+    of length k+1.
+    """
+    outputs = []
+
+    iters = [iter(x) for x in baskets.itervalues()]
+    si = itertools.chain(*iters)
+    C = frozenset(si)
+
+    print C
+
+if __name__ == '__main__':
+    baskets = {
+        'A': {4, 5, 2},
+        'B': {7, 2, 3},
+        'C': {3, 2, 1, 4, 7},
+        'D': {2, 4, 5, 7},
+        'E': {1, 2, 5},
+        'F': {2, 4, 6, 7},
+        'G': {1, 2, 3, 4, 5},
+        'H': {2, 5, 6, 7},
+    }
+
+    apriori(baskets, .2)
