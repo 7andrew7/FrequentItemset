dd77df41b58a9ad31bdf818207422f86e2359422
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-18 14:51:05 -0800

    dummy test case

---
+++ apriori_test.cpp
@@ -0,0 +1,23 @@
+#include "apriori.h"
+
+#include "gtest/gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+
+// TEST(Apriori, BasicTest) {
+//     basket_vector_t input = {
+//         {2, 5, 6},
+//         {2, 3, 7, 8},
+//         {1, 2, 3, 4, 7},
+//         {2, 4, 7, 9},
+//         {1, 2, 5, 9},
+//         {2, 4, 6, 7},
+//         {1, 2, 3, 5, 7},
+//         {2, 5, 6, 7}
+//     };
+
+//     basket_vector_t result{apriori(input, 2)}
+// }
