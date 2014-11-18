dd5df4bbb72e1ff67f256d5777360013b3229f06
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-17 17:45:50 -0800

    compute frequent singletons in C++

--- apriori.cpp
+++ apriori.cpp
@@ -7,18 +7,30 @@
 basket_vector_t apriori(const basket_vector_t &baskets, std::size_t support) {
     assert(support <= baskets.size());
 
-    // Compute frequent singletons
-    std::unordered_map<item_t, std::size_t> counts{}
+    // Step #1: Compute frequent singletons
+
+    std::unordered_map<item_t, std::size_t> counts{};
     for (const basket_t &basket : baskets) {
-        for (const item_t item : basket) {
-            counts[item_t]++;
+        for (item_t item : basket) {
+            counts[item]++;
         }
     }
 
-    
     basket_vector_t output{};
+    for (auto it = counts.cbegin(); it != counts.cend(); ++it) {
+        if (it->second >= support) {
+            output.push_back(basket_t{it->first});
+        }
+    }
 
-    return baskets;
+    // auto prev_begin = output.cbegin();
+    // auto prev_end = output.cend();
+
+    // while(true) {
+    //     auto candidates = candidate_gen(prev_begin, prev_end);
+    // }
+    
+    return output;
 }
 
 template<class T>
@@ -42,11 +54,11 @@ std::ostream& operator<<(std::ostream &out, const std::vector<T> &vec) {
 
 int main () {
     basket_vector_t input = {
-        {2, 5, 5},
+        {2, 5, 6},
         {2, 3, 7, 8},
         {1, 2, 3, 4, 7},
-        {2, 4, 5, 7},
-        {1, 2, 5},
+        {2, 4, 7, 9},
+        {1, 2, 5, 9},
         {2, 4, 6, 7},
         {1, 2, 3, 7, 5},
         {2, 5, 6, 7}
