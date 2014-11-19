7b8072eb10eba6181f901fe707913e0563c24340
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-19 14:17:29 -0800

    script to fetch dependencies

---
+++ fetch_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d "gtest-1.7.0" ];
+then
+    echo "Skipping gtest installation"
+else
+    echo "Fetching gtest"
+    wget https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    unzip gtest-1.7.0.zip
+fi
+
+if [ -d "data" ];
+then
+    echo "Skipping data installation"
+else
+    echo "Installing data dependencies"
+    mkdir data
+    wget https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data -o ./data/agaricus-lepiota.data
+fi
