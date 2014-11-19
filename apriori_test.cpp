9d3601084308853698b8ceb4b0e7f7816cadc8fa
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-19 08:34:15 -0800

    Compute expected test results using brute force search

--- apriori_test.cpp
+++ apriori_test.cpp
@@ -9,6 +9,36 @@ TEST(sample_test_case, sample_test)
     EXPECT_EQ(1, 1);
 }
 
+/**
+ * Brute force implementation of frequent itemset mining
+ */
+std::unordered_set<basket_t> brute_force_frequent_items(
+    const std::unordered_set<basket_t> &input,
+    std::size_t support,
+    std::size_t range)
+{
+    std::unordered_set<basket_t> output{};
+
+    auto max_val = (1 << range);
+    for (std::size_t i = 1; i < max_val; ++i) {
+        basket_t candidate{};
+        for (std::size_t j = 0 ; j < range; ++j) {
+            if ((i>>j) & 1)
+                candidate.push_back(j + 1);
+        }
+
+        std::size_t count = 0;
+        for (const basket_t &basket : input) {
+            if (contains(candidate, basket)) {
+                if (++count == support)
+                    output.insert(candidate);
+            }
+        }
+    }
+
+    return output;
+}
+
 TEST(Apriori, BasicTest) {
 
     const std::unordered_set<basket_t> input {
@@ -25,42 +55,6 @@ TEST(Apriori, BasicTest) {
     basket_vector_t result{apriori(input.cbegin(), input.cend(), 2)};
     const std::unordered_set<basket_t> result_set{result.cbegin(), result.cend()};
 
-    const std::unordered_set<basket_t> expected_set
-    {
-        {1},
-        {2},
-        {3},
-        {4},
-        {5},
-        {6},
-        {7},
-        {9},
-        {4,7},
-        {1,3},
-        {5,6},
-        {3,7},
-        {2,5},
-        {1,2},
-        {2,9},
-        {1,5},
-        {2,6},
-        {2,3},
-        {6,7},
-        {2,7},
-        {5,7},
-        {1,7},
-        {2,4},
-        {1,3,7},
-        {2,4,7},
-        {1,2,7},
-        {2,6,7},
-        {2,5,7},
-        {1,2,3},
-        {2,5,6},
-        {2,3,7},
-        {1,2,5},
-        {1,2,3,7},
-    };
-
+    const auto expected_set = brute_force_frequent_items(input, 2, 9);
     EXPECT_EQ(result_set, expected_set);
 }
