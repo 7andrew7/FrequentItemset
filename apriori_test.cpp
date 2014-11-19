d48488649413d041116a05ddfe9628bbc2bab90e
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-19 06:54:51 -0800

    Complete unit test of apriori c++

--- apriori_test.cpp
+++ apriori_test.cpp
@@ -23,4 +23,44 @@ TEST(Apriori, BasicTest) {
     };
 
     basket_vector_t result{apriori(input.cbegin(), input.cend(), 2)};
+    const std::unordered_set<basket_t> result_set{result.cbegin(), result.cend()};
+
+    const std::unordered_set<basket_t> expected_set
+    {
+        {1},
+        {2},
+        {3},
+        {4},
+        {5},
+        {6},
+        {7},
+        {9},
+        {4,7},
+        {1,3},
+        {5,6},
+        {3,7},
+        {2,5},
+        {1,2},
+        {2,9},
+        {1,5},
+        {2,6},
+        {2,3},
+        {6,7},
+        {2,7},
+        {5,7},
+        {1,7},
+        {2,4},
+        {1,3,7},
+        {2,4,7},
+        {1,2,7},
+        {2,6,7},
+        {2,5,7},
+        {1,2,3},
+        {2,5,6},
+        {2,3,7},
+        {1,2,5},
+        {1,2,3,7},
+    };
+
+    EXPECT_EQ(result_set, expected_set);
 }
