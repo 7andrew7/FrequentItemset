ff579c4b0566b4121a83c2ff268a7f47adc20b1b
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-19 11:22:59 -0800

    Manually order the inputs to apriori

--- apriori_test.cpp
+++ apriori_test.cpp
@@ -13,7 +13,7 @@ TEST(sample_test_case, sample_test)
  * Brute force implementation of frequent itemset mining
  */
 std::unordered_set<basket_t> brute_force_frequent_items(
-    const std::unordered_set<basket_t> &input,
+    const basket_set_t &input,
     std::size_t support,
     std::size_t range)
 {
@@ -48,15 +48,15 @@ TEST(Apriori, Contains) {
 
 TEST(Apriori, BasicTest) {
 
-    const std::unordered_set<basket_t> input {
+    const basket_set_t input {
         {2, 5, 6},
-        {2, 3, 7, 8},
-        {1, 2, 3, 4, 7},
-        {2, 4, 7, 9},
         {1, 2, 5, 9},
+        {2, 3, 7, 8},
         {2, 4, 6, 7},
-        {1, 2, 3, 5, 7},
-        {2, 5, 6, 7}
+        {2, 4, 7, 9},
+        {2, 5, 6, 7},
+        {1, 2, 3, 4, 7},
+        {1, 2, 3, 5, 7}
     };
 
     const basket_set_t result{apriori(input.cbegin(), input.cend(), 2)};
