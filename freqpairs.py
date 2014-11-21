4ab34357a7f031d035aa412b575153e9437b2d2f
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-21 14:53:04 -0800

    Aprioiri implementation of freqpairs

--- freqpairs.py
+++ freqpairs.py
@@ -12,8 +12,16 @@ def freqpairs_brute_force(it, support):
     counter = Counter((x, y) for tpl in it for x, y in combinations(tpl, 2))
     return (key for key, val in counter.iteritems() if val >= support)
 
+def freqpairs_apriori(it, support):
+    counter1 = Counter(x for tpl in it for x in tpl)
+    l1 = (key for key, val in counter1.iteritems() if val >= support)
+
+    c2 = set(combinations(l1, 2))
+    counter2 = Counter((x,y) for tpl in it for x, y in combinations(tpl, 2) if (x, y) in c2)
+    return (key for key, val in counter2.iteritems() if val >= support)
+
 if __name__ == '__main__':
-    fp = freqpairs_brute_force([
+    _in = [
         (2, 5, 6),
         (2, 3, 7, 8),
         (1, 2, 3, 4, 7),
@@ -22,5 +30,7 @@ if __name__ == '__main__':
         (2, 4, 6, 7),
         (1, 2, 3, 5, 7),
         (2, 5, 6, 7)
-    ], 2)
+    ]
+
+    fp = freqpairs_apriori(_in, 2)
     print sorted(list(fp))
