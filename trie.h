52c1a1276ceeb93ffac7137312770996e256534c
Andrew Whitaker <whitaker@cs.washington.edu>
2014-11-28 09:59:51 -0800

    Delete dead code

--- trie.h
+++ trie.h
@@ -21,28 +21,6 @@ public:
         assert(child);
         child->_count++;
     }
-    /**
-     * Add an element to the Trie with zero count.
-     */
-    template<class ForwardIterator>
-    void add_element(ForwardIterator begin, ForwardIterator end)
-    {
-        if (begin == end)
-            return;
-        item_t first = *begin;
-        auto child = _map[first];
-        if (child == nullptr) {
-            // allocate
-            _map[first] = child;
-        }
-        child->add_element(begin + 1, end);
-    }
-
-    template<class Container>
-    void add_element(const Container &c)
-    {
-        add_element(c.cbegin(), c.cend());
-    }
 
     /**
      * Increment the count of an existing Trie element; has not effect for
